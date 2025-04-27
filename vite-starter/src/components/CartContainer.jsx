ee2d3878f8116d77c49cd0e67efab70c57e873a4
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-04-27 12:31:47 +0300

    feat: finish the presentational UI for both CartContainer and CartItem components

--- vite-starter/src/components/CartContainer.jsx
+++ vite-starter/src/components/CartContainer.jsx
@@ -1,9 +1,17 @@
+import { useSelector } from 'react-redux'
 import CartItem from './CartItem'
 
 function CartContainer() {
+  // const {cartItems} = useSelector((store) => store.mobilePhonesCart)
+  const {cartItems, total} = useSelector((store) => store.mobilePhonesCart)  
+  
   return (
     <div className='cart'>
+      <header>
       <h2>Your bag</h2>
+      </header>
+      {cartItems.map((item) => <CartItem key={item.id} {...item}/>)}
+      <hr />
       <footer>
         <div className="cart-total">
           <h4>
@@ -11,10 +19,11 @@ function CartContainer() {
             Total
             </span>
             <span>
-              $ 0
+              ${total}
             </span>
           </h4>
         </div>
+        <button className="btn clear-btn">clear cart</button>
       </footer>
     </div>
   )
--- vite-starter/src/components/CartItem.jsx
+++ vite-starter/src/components/CartItem.jsx
@@ -1,6 +1,33 @@
-function CartItem() {
-  return (
-    <div>CartItem</div>
-  )
+import { ChevronDown, ChevronUp } from '../icons'
+
+function CartItem({ title, price, img, amount }) {
+	return (
+		<article className='cart-item'>
+			<img
+				src={img}
+				alt={title}
+			/>
+			<section>
+				<h4>{title}</h4>
+				<p className='item-price'>${price}</p>
+				<button className='remove-btn'>remove</button>
+			</section>
+
+			<section>
+        <div>
+          <button className="amount-btn">
+          <ChevronUp />
+          </button>
+          {/* At first, I put the amount in a span, this lead to a broken design
+          since we needed an element of type block to make the chevrons appear
+          in the expected view .  */}
+          <p className='amount'>{amount}</p>
+          <button className="amount-btn">
+          <ChevronDown />
+            </button>
+        </div>
+      </section>
+		</article>
+	)
 }
 export default CartItem
