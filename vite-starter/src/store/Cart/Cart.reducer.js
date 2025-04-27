df61f3e45dd327d88e15fdfbf95800014ce18882
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-04-27 15:01:41 +0300

    feat: add crud functionality to the cart

--- vite-starter/src/store/Cart/Cart.reducer.js
+++ vite-starter/src/store/Cart/Cart.reducer.js
@@ -1,7 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import cartItems from '../../cartItems';
 
-
 const initialState = {
   cartItems,
   amount: 4,
@@ -18,9 +17,35 @@ const cartSlice = createSlice({
   reducers: {
     clearCart: (store) => {
       console.log("### Cart was cleared ! ###");
+      // We can mutate the state directly because behind the scenes immer takes
+      // care of returning a new copy of the state
+      store.cartItems = []
+      console.log(store.cartItems);
+    },
+    removeItem: (store, {payload}) => {
+      const cartItems = store.cartItems.filter((item) => item.id !== payload)
+      store.cartItems = cartItems
+    },
+    increaseItemQty: (store, {payload}) => {
+      const item = store.cartItems.find((item) => item.id === payload)
+      item.amount = item.amount + 1
+    },
+    decreaseItemQty: (store, {payload}) => {
+      const item = store.cartItems.find((item) => item.id === payload)
+      console.log(item);
+      console.log(payload);
+      console.log(item.amount);
       
-      store.mobilePhonesCart = []
-    }
+      if(item.amount <= 1) {
+        /* The below code won't work simply because you can't call a reducer within another one . 
+        
+        You should implement the logic of removing the item yourself*/
+        // removeItem(payload)
+        store.cartItems = store.cartItems.filter((item) => item.id !== payload)
+        return
+      }
+      item.amount = item.amount - 1
+    },
   }
 })
 console.log('### This is cart slice ###');
@@ -30,5 +55,5 @@ console.log(cartSlice);
 
 export const cartReducer = cartSlice.reducer
 // This is an action creator
-export const {clearCart} = cartSlice.actions
- 
+export const {clearCart, removeItem, increaseItemQty, decreaseItemQty} = cartSlice.actions
+
