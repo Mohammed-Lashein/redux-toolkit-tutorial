6840eebcc85c5bf949b61b5d4aa3f8ec9f36ed67
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-04-28 07:00:26 +0300

    feat: delegate cart clearance to the modal

--- vite-starter/src/components/CartContainer.jsx
+++ vite-starter/src/components/CartContainer.jsx
@@ -2,8 +2,8 @@ import { useDispatch, useSelector } from "react-redux"
 import CartItem from "./CartItem"
 import { calculateTotalItemsCount, calculateTotalPrice, clearCart } from "../store/Cart/Cart.reducer"
 import { useEffect } from "react"
-import Modal from './Modal'
-import { openModal } from '../store/Modal/Modal.reducer'
+import Modal from "./Modal"
+import { openModal } from "../store/Modal/Modal.reducer"
 
 // should be extracted to a utils file, but since our application is small, there is no problem in
 // leaving it here
@@ -12,21 +12,21 @@ function formatPrice(price) {
 }
 
 function CartContainer() {
-  // selectors 
+	// selectors
 	const { cartItems, total, amount } = useSelector((store) => store.mobilePhonesCart)
-  const {isModalOpened} = useSelector((store) => store.modalVisibility)
-	const dispatch = useDispatch()  
+	const { isModalOpened } = useSelector((store) => store.modalVisibility)
+	const dispatch = useDispatch()
 
-  // calculate total price and items count
+	// calculate total price and items count
 	useEffect(() => {
 		dispatch(calculateTotalItemsCount())
 		dispatch(calculateTotalPrice())
 	}, [cartItems])
 
-  function handleCartClearance() {
-    dispatch(clearCart())
-    dispatch(openModal())
-  }
+	function handleCartClearance() {
+		// dispatch(clearCart())
+		dispatch(openModal())
+	}
 
 	if (amount < 1) {
 		return (
@@ -65,7 +65,7 @@ function CartContainer() {
 					clear cart
 				</button>
 			</footer>
-      {isModalOpened && <Modal />}
+			{isModalOpened && <Modal />}
 		</div>
 	)
 }
--- vite-starter/src/components/Modal.jsx
+++ vite-starter/src/components/Modal.jsx
@@ -1,12 +1,27 @@
+import { useDispatch } from "react-redux"
+import { clearCart } from "../store/Cart/Cart.reducer"
+import { closeModal } from "../store/Modal/Modal.reducer"
+
 function Modal() {
+	const dispatch = useDispatch()
 	return (
 		<div className='modal-container'>
 			<div className='modal'>
 				<h4>Remove all items from your shopping cart ?</h4>
 				<div className='btn-container'>
 					{/* I swapped the styles because confirming deletion is supposed to be the danger action not the cancel */}
-					<button className='btn clear-btn'>confirm</button>
-					<button className='btn confirm-btn'>cancel</button>
+					<button
+						className='btn clear-btn'
+						onClick={() => dispatch(clearCart())}
+					>
+						confirm
+					</button>
+					<button
+						className='btn confirm-btn'
+						onClick={() => dispatch(closeModal())}
+					>
+						cancel
+					</button>
 				</div>
 			</div>
 		</div>
